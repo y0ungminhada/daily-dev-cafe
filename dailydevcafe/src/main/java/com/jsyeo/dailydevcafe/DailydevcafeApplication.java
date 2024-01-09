81933ec7cbbf39a723e120b448aa26475f11ba8e
jsyeo97 <jsyeo97@naver.com>
2024-01-09 18:03:14 +0900

    backend init

---
+++ dailydevcafe/src/main/java/com/jsyeo/dailydevcafe/DailydevcafeApplication.java
@@ -0,0 +1,13 @@
+package com.jsyeo.dailydevcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailydevcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailydevcafeApplication.class, args);
+	}
+
+}
